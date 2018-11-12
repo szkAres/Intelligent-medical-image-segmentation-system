142d14e5c7b224c90f9de14847108fd9dcf3531b
szk <szk6225783szk@126.com>
2018-11-12 08:24:59 +0800

    Revert "Merge branch 'master' of https://github.com/szkAres/Intelligent-medical-image-segmentation-system"
    This reverts commit 84a1331d1bb8c47b587f754013b2518f344662ee, reversing
    changes made to 95b8c3ddce4cd421912b3d346b70f9062b631391.

--- img_sql_database/image_table_test.py
+++
@@ -1,7 +0,0 @@
-from image_table import image_table
-
-image_table_test = image_table('first_flask','ImagesDatabase')
-image_table_test.connect_to_database()
-image_table_test.check_by_type('fat',True)
-image_table_test.check_by_user('Allen',True)
-image_table_test.disconnect_database()
