0116003c7b78b3d4b52534e86e4e8af918180f6c
szk <szk6225783szk@126.com>
2018-11-22 14:05:49 +0800

    船新版本
    更新界面，腹部脂肪自动分割，优化存储管理

---
+++ flask_web_without_sql/Class_Predict.py
@@ -0,0 +1,44 @@
+import cv2
+from keras.preprocessing.image import img_to_array
+from myUnet import *
+
+
+class MyPredict():
+    def __init__(self):
+        self.basepath = os.path.dirname(__file__)
+        self.Imgs_Test = np.ndarray((1, 512, 512, 1), dtype=np.float32)
+        self.Predict = np.ndarray((512, 512), dtype=np.float32)
+
+    def LoadPic(self):
+        self.img = cv2.imread(self.basepath + "/static/auto_photos/auto_picture.jpg", cv2.IMREAD_GRAYSCALE)
+
+       # self.img = cv2.imread(self.basepath + "/static/photos/test.jpg", cv2.IMREAD_GRAYSCALE)
+
+    def PredictPic(self):
+        pix = np.float32(self.img)
+        max = np.max(pix)
+        pix = np.divide(pix, max)
+        pix = img_to_array(pix)
+        self.Imgs_Test[0] = pix
+
+        unet = myUnet()
+        model = unet.Model
+        model.load_weights('unet6.hdf5')
+
+        self.Result = model.predict(self.Imgs_Test, verbose=1)
+
+    def SavePic(self):
+        for m in range(512):
+            for n in range(512):
+                if ((self.Result[0][m][n][0] >= self.Result[0][m][n][1]) & (self.Result[0][m][n][0] >= self.Result[0][m][n][2])):
+                    self.Predict[m][n] = 0
+                elif (self.Result[0][m][n][1] >= self.Result[0][m][n][2]):
+                    self.Predict[m][n] = 122
+                else:
+                    self.Predict[m][n] = 255
+
+        # cv2.imwrite(self.basepath + 'after_auto_belly_temp.jpg', self.Predict)
+
+        path = self.basepath + "/static/after_auto_belly_photo/"
+        cv2.imwrite(os.path.join(path, 'after_auto_belly_temp.jpg'), self.Predict)
+
--- flask_web_without_sql/myUnet.py
+++ flask_web_without_sql/myUnet.py
@@ -7,24 +7,6 @@ import numpy as np
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-import tensorflow as tf
-from keras import backend as K
-
-num_cores = 4
-
-#if GPU:
-#num_GPU = 1
-#num_CPU = 1
-#if CPU:
-num_CPU = 1
-num_GPU = 0
-
-config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,\
-        inter_op_parallelism_threads=num_cores, allow_soft_placement=True,\
-        device_count = {'CPU' : num_CPU, 'GPU' : num_GPU})
-session = tf.Session(config=config)
-K.set_session(session)
-
 class myUnet():
     def __init__(self):
         self.Path = os.path.dirname(os.path.realpath(__file__))
