1f97d4ad87141e65ecea4101bb60d7a641597a7a
szk <szk6225783szk@126.com>
2018-12-03 10:39:39 +0800

    窗宽窗位调整

---
+++ flask_web_with_sql/DicomProcess.py
@@ -0,0 +1,51 @@
+import pydicom
+import os
+import numpy as np
+import cv2
+
+DefaultCenter = 0 #For IM0.dcm, DefaultCenter = 352.5
+DefaultWidth = 0  #For IM0.dcm, DefaultWidth = 705.0
+
+class MyDicomPreProcess():
+    def __init__(self):
+        self.basepath = os.path.dirname(__file__)
+
+    def ImportPic(self):
+        self.OriginalPic = pydicom.read_file(self.basepath + '//testDCM//IM0.dcm')
+        self.Pixel = self.OriginalPic.pixel_array
+        self.Pixel = np.float32(self.Pixel)
+
+        global DefaultCenter,DefaultWidth
+        DefaultCenter = 0.5 * ( np.max(self.Pixel) + np.min(self.Pixel) )
+        DefaultWidth = np.max(self.Pixel) - np.min(self.Pixel)
+
+    def ChangeWindowCenterAndWidth(self, Center=-1, Width=-1):
+        global DefaultWidth,DefaultCenter
+        if(Center<0):
+            Center = DefaultCenter
+        if(Width<0):
+            Width = DefaultWidth
+
+        size = self.Pixel.shape
+        self.ChangedPic = np.ndarray((512, 512), dtype=np.float32)
+        min = (2 * Center - Width) / 2.0
+        max = (2 * Center + Width) / 2.0
+        for i in range(size[0]):
+            for j in range(size[1]):
+                if(self.Pixel[i][j] < min):
+                    self.ChangedPic[i][j] = 0
+                elif(self.Pixel[i][j] > max):
+                    self.ChangedPic[i][j] = 255
+                else:
+                    self.ChangedPic[i][j] =(self.Pixel[i][j] - min)*255/(max-min)
+
+    def SavePic(self):
+        cv2.imwrite(self.basepath+'//Center&WidthChanged.jpg', self.ChangedPic)
+
+if __name__ =='__main__':
+    myPreProcess = MyDicomPreProcess()
+    myPreProcess.ImportPic()
+    #myPreProcess.ChangeWindowCenterAndWidth()    #使用覆盖所有像素范围的default窗宽窗位
+    myPreProcess.ChangeWindowCenterAndWidth(Center=500,Width=500) #指定窗宽窗位
+    myPreProcess.SavePic()
+
