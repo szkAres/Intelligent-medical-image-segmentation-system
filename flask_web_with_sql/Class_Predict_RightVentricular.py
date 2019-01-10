9ed087779c8030d00e829c78be751eb21f215c7e
szk <szk6225783szk@126.com>
2019-01-10 22:19:09 +0800

    bugFix

--- flask_web_with_sql/Class_Predict_RightVentricular.py
+++ flask_web_with_sql/Class_Predict_RightVentricular.py
@@ -102,7 +102,7 @@ class MyPredictRightVentricular():
 
     def LoadPic(self):
         self.img = cv2.imread(self.basepath + "/static/auto_photos/auto_picture.jpg", cv2.IMREAD_GRAYSCALE)
-        self.img = cv2.resize(self.img, (256,216), interpolation=cv2.INTER_NEAREST)
+        self.img = cv2.resize(self.img, (216,256), interpolation=cv2.INTER_NEAREST)
         pix = np.float32(self.img)
         max = np.max(pix)
         pix = np.divide(pix, max)
